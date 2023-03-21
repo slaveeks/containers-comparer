c8ae2d8d1d6d0b33b2104d927fc63b0221b6cfc2
slaveeks <chernyshev2001@icloud.com>
2023-03-21 14:31:38 +0300

    Updated getting memory usage

--- src/marks/ram.go
+++ src/marks/ram.go
@@ -3,11 +3,18 @@ package marks
 import (
 	"fmt"
 	"os/exec"
+	"strings"
 )
 
 func GetCurrentMemoryUsage() {
 	cmd := exec.Command("free")
 
 	output, _ := cmd.CombinedOutput()
-	fmt.Println(string(output))
+
+	for n, line := range strings.Split(strings.TrimSuffix(string(output), "\n"), "\n") {
+		if n == 1 {
+			a := strings.Fields(line)
+			fmt.Println(a[2])
+		}
+	}
 }
